f239c068313e0915303b88cd59737256bba3c3c7
Kevin Jenson <kjjenson@gmail.com>
2018-01-16 10:51:34 -0700

    initial pattern setup

--- server/server.js
+++ server/server.js
@@ -1,26 +1,52 @@
 require('dotenv').config()
-const express = require('express')
-    , bodyParser = require('body-parser')
-    , cors = require('cors')
-    , session = require('express-session')
-    , massive = require('massive')
-    , axios = require('axios')
+const express = require('express'),
+  bodyParser = require('body-parser'),
+  cors = require('cors'),
+  session = require('express-session'),
+  massive = require('massive'),
+  axios = require('axios')
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors())
 
-massive(process.env.DB_CONNECTION).then(db => {
-    app.set('db',db)
+massive(process.env.DB_CONNECTION).then((db) => {
+  app.set('db', db)
 })
 
 app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
 }))
 
-ap.use(express.static(__dirname+ '/../build'))
 
-app.listen(process.env.SERVER_PORT, ()=>{ console.log(`Server listening on port ${process.env.SERVER_PORT}`)})
+app.use(express.static(`${__dirname}/../build`))
+
+app.listen(process.env.SERVER_PORT, () => { console.log(`Server listening on port ${process.env.SERVER_PORT}`) })
+
+
+/*
+decorator pattern
+
+pointless.js file:
+
+pointless: () => {
+    const db = app.get('db')
+    db.users([1, 2, 3]).then(res => {
+        res.status(200).send(res)
+    })
+}
+
+module.exports = function(app) {
+    app.get('/api/users', pointless)
+}
+
+
+server.js file:
+
+const pointless = require('./pointless')
+
+pointless(app)
+*/
--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
-import promiseMiddleware from 'redux-promise-middleware'
+import thunk from 'redux-thunk'
 import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import reducer from './ducks/reducer'
 
-export default createStore(reducer, applyMiddleware(promiseMiddleware()))
+export default createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
 
