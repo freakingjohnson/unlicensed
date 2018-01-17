20ec637fa8cb637008f3a9444d5685dc966575c4
Wesley Fukui <wes.fukui@gmail.com>
2018-01-17 12:11:05 -0700

    stupid styling

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -6,7 +6,10 @@ import Button from 'material-ui/Button';
 import Typography from 'material-ui/Typography';
 import Avatar from 'material-ui/Avatar'
 
-
+const style = {
+  padding: 0,
+  margin: 0,
+}
 const styles = theme => ({
   card: {
     minWidth: 275,
@@ -25,15 +28,22 @@ const styles = theme => ({
   avatar: {
     width: 60,
     height: 60,
-    display: 'flex',
-    justifyContent: 'center',
   },
   pic: {
     maxWidth: 345,
   },
   media: {
     height: 'auto',
-    width: '100px',
+    width: '100vw',
+  },
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+  },
+  button: {
+    margin: 0,
+    padding: 0,
   },
 });
 
@@ -49,9 +59,7 @@ function Profile(props) {
     <div key={jobs.photo}>
       <Card >
         <CardMedia>
-          <div className={classes.media}>
-            <img src={jobDesc.photo} alt="job pic" />
-          </div>
+          <img className={classes.media} src={jobDesc.photo} alt="job pic" />
         </CardMedia>
         <CardContent>
           <Typography component="p">
@@ -65,14 +73,16 @@ function Profile(props) {
   return (
 
     <div>
-      <Avatar alt="profile pic" src="https://cdn.shopify.com/s/files/1/1325/1409/products/157-Mexico-Flag-Solo_Single_Front_grande_33205e4d-5e4f-4df4-919b-74f1d7de4f14_1024x1024.png?v=1476740848" className={classes.avatar} />
+      <div className={classes.row}>
+        <Avatar alt="profile pic" src="https://cdn.shopify.com/s/files/1/1325/1409/products/157-Mexico-Flag-Solo_Single_Front_grande_33205e4d-5e4f-4df4-919b-74f1d7de4f14_1024x1024.png?v=1476740848" className={classes.avatar} />
+      </div>
       <Card className={classes.card}>
         <CardContent>
           <Typography className={classes.title}>Name</Typography>
           <Typography className={classes.title}>Services Provided</Typography>
-          <CardActions>
+          <CardActions style={style} >
             {/* write logic that will display button if more than 3 services provided */}
-            <Button dense>More Services</Button>
+            <Button style={style} dense>More Services</Button>
           </CardActions>
           <Typography className={classes.title}>Desc</Typography>
           <Typography className={classes.title}>Contact Info</Typography>
