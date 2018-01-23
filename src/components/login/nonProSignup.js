82f1a613f55cdbe34f81dbdab4632653e3183d91
freakingjohnson <enjayification@gmail.com>
2018-01-23 09:43:13 -0700

    sign  up non pro working

--- src/components/login/nonProSignup.js
+++ src/components/login/nonProSignup.js
@@ -6,46 +6,42 @@ import { TextField, Button, FormGroup } from 'material-ui'
 import { setInfo } from '../../ducks/reducers/loginReducer'
 
 const nonProSignup = ({
-  loginReducer, email, password, verifyPassword, setInfo, history,
-}) => {
-  const nonProInfo = loginReducer
-  return (
-    <div> Enter your email and password
-      <FormGroup>
-        <TextField
-          label="Email"
-          name="email"
-          value={email}
-          onChange={e => setInfo(e)}
-        />
-        <TextField
-          label="Password"
-          name="password"
-          type="password"
-          maxLength="4"
-          value={password}
-          onChange={e => setInfo(e)}
-        />
-        <TextField
-          label="Verify Password"
-          name="verifyPassword"
-          type="password"
-          error={password !== verifyPassword}
-          value={verifyPassword}
-          onChange={e => setInfo(e)}
-        />
-      </FormGroup>
-      <Button
-        raised
-        color="primary"
-        disabled={!email || !password || !verifyPassword || password !== verifyPassword}
-        onClick={() => send(email, password, history)}
-      >
+  email, password, verifyPassword, setInfo, history,
+}) => (
+  <div> Enter your email and password
+    <FormGroup>
+      <TextField
+        label="Email"
+        name="email"
+        value={email}
+        onChange={e => setInfo(e)}
+      />
+      <TextField
+        label="Password"
+        name="password"
+        type="password"
+        value={password}
+        onChange={e => setInfo(e)}
+      />
+      <TextField
+        label="Verify Password"
+        name="verifyPassword"
+        type="password"
+        error={password !== verifyPassword}
+        value={verifyPassword}
+        onChange={e => setInfo(e)}
+      />
+    </FormGroup>
+    <Button
+      raised
+      color="primary"
+      disabled={!email || !password || !verifyPassword || password !== verifyPassword}
+      onClick={() => send(email, password, history)}
+    >
     Signup
-      </Button>
-    </div>
-  )
-}
+    </Button>
+  </div>
+)
 
 const send = (email, password, history) => {
   axios.post('api/addNonPro', { email, password }).then((res) => {
@@ -63,7 +59,6 @@ const mapStateToProps = state => ({
 export default connect(mapStateToProps, { setInfo })(nonProSignup)
 
 nonProSignup.propTypes = {
-  loginReducer: PropTypes.object.isRequired,
   email: PropTypes.string.isRequired,
   password: PropTypes.string.isRequired,
   verifyPassword: PropTypes.string.isRequired,
