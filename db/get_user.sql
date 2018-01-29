f39d69ec6fcae98caf13b62637b5252422c61edd
freakingjohnson <enjayification@gmail.com>
2018-01-29 12:34:02 -0700

    fixed so that password isn't loaded to state

--- db/get_user.sql
+++ db/get_user.sql
@@ -1,4 +1,4 @@
-select users.*,
+select id, first_name, last_name, phone, email, bio_info, profile_photo, location,
    array_to_string( array_agg( distinct worktype.worktype ), ', ' ) as worktype,
    array_to_string( array_agg( distinct workphotos.photo ), ', ' ) as workphotos,
    array_to_string( array_agg( distinct workphotos.photo_info ), ', ') as photo_info
