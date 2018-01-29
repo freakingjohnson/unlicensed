c7a8ebf0a03ddb4f8f9d7c351ceeeeb3ce33c3f8
freakingjohnson <enjayification@gmail.com>
2018-01-29 12:37:43 -0700

    fixed get_user and schema so password doesn't get pulled on mount

--- db/schemas/users.sql
+++ db/schemas/users.sql
@@ -7,5 +7,6 @@ create table users (
     bio_info text,
     profile_photo text DEFAULT 'http://www.personalbrandingblog.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640-300x300.png',
     location text,
-    user_password text
+    user_password text,
+    stripe_id text,
 )
